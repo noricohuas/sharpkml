8503a2dd3a7c49a090d4a3d77d55ccfd6bf642ac
John Teague <jcteague@gmail.com>
2016-02-22 16:34:42 -0600

    track test

---
+++ UnitTests/Engine/TrackKmlTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using SharpKml.Dom;
+using SharpKml.Dom.GX;
+using SharpKml.Engine;
+
+namespace UnitTests.Engine
+{
+    [TestFixture]
+    public class TrackKmlTests
+    {
+        [Test]
+        public void should_load_track_geometry()
+        {
+            using (var stream = SampleData.CreateStream("Engine.Data.Tracks.kml"))
+            {
+
+                KmlFile file = KmlFile.Load(stream,false);
+                var placemark = file.FindObject("geoffrey") as Placemark;
+                var track = placemark.Geometry as Track;
+                Assert.That(track,Is.Not.Null);
+                Assert.That(track.When.Count(), Is.EqualTo(track.Coordinates.Count()));
+            }
+        }
+    }
+}
